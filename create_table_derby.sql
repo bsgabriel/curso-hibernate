12e7547c90dbb3acd5998e9d96e06e90a829f81f
bsgabriel <gabrielbs22@icloud.com>
2023-01-02 16:45:01 -0300

    Atualizado script de criação de tabela

--- create_table_derby.sql
+++ create_table_derby.sql
@@ -2,6 +2,12 @@ CREATE TABLE ALUNOS (
 	cod_aluno INTEGER PRIMARY KEY,
 	nome VARCHAR(40),
 	curso VARCHAR(100),
-	telefone VARCHAR(11),
 	cidade VARCHAR(100)
+);
+
+CREATE TABLE telefones_alunos (
+	cod_aluno INTEGER NOT NULL,
+	telefone VARCHAR(11) NOT NULL,
+	CONSTRAINT pk_aluno_telefone PRIMARY KEY (cod_aluno, telefone),
+	CONSTRAINT fk_aluno_telefone FOREIGN KEY (cod_aluno) REFERENCES alunos (cod_aluno)
 );
